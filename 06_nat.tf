10b50e82f6077f7717c485d6e697649cd87d51d9
OPUS <pyopyo6845@gmail.com>
2021-11-28 21:49:04 +0900

    'step1'

---
+++ 06_nat.tf
@@ -0,0 +1,7 @@
+resource "aws_eip" "Hong_ngw_ip" {
+    vpc = true
+}
+resource "aws_nat_gateway" "Hong_ngw" {
+    allocation_id = aws_eip.Hong_ngw_ip.id
+    subnet_id = aws_subnet.Hong_Prv_a.id
+}
